ea76ef012b86c3f1441d9d55e9254a01e91f5f30
Toons-dev <contact.debuire@gmail>
2019-12-12 14:50:04 +0100

    template view

--- public/views/template.php
+++ public/views/template.php
@@ -7,36 +7,52 @@
         <title><?= isset($title) ? $title : WEBSITE_TITLE ?></title>
 
         <!-- Bootstrap core CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="<?= css_url('bootstrap.min.css'); ?>" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville|Roboto+Mono&display=swap" rel="stylesheet">
         <link href="<?= css_url('style.css'); ?>" rel="stylesheet">
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="container">
-                    <a class="navbar-brand" href="<?= BASE_URL; ?>">
-                        <img src="<?= img_url('logoBlacLeft.png'); ?>" style="width: 130px;" alt="">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <a href="<?= BASE_URL; ?>">
+                    <img src="<?= img_url('logoBlancLong.png'); ?>" style="width: 140px;" alt="Logo LightCards">
+                </a>
+            <div class="menu d-flex justify-content-end">
+                <nav class="navbar navbar-expand-lg">
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon">+</span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarText">
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="navbutton" href="<?= url('explorer') ?>">Explorer</a>
+                            <li class="nav-item active">
+                                <a class="astyle nav-link" href="<?= url('explorer') ?>">Explorer<span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="navbutton" href="<?= url('connexion') ?>">Connexion</a>
+                            <a class="astyle nav-link" href="<?= url('connexion') ?>">Connexion</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="astyle nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catégorie</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="astyle dropdown-item" href="#">Action</a>
+                                    <a class="astyle dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                    <a class="astyle dropdown-item" href="#">Something else here</a>
+                                </div>
                             </li>
                         </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                        </form>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </header>
 
         <main role="main">
         <div class="album py-5">
-            <div class="container" style="color: white;">
+            <div class="container">
                 <?php
                     // contenu de la page
                     echo $content; 
@@ -45,16 +61,13 @@
         </div>
         </main>
 
-        <footer style="background-color: black;">
+        <footer>
             <div class="container">
-            <p class="float-right">
-                
-            </p>
-            <p>2019 &copy; </p>
+            <p>LightCards 2019 &copy; </p>
             </div>
         </footer>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="<?= js_url('jquery-3.4.1.min.js'); ?>"></script>
+        <script src="<?= js_url('bootstrap.min.js'); ?>"></script>
         <script src="<?= js_url('script.js'); ?>"></script>
     </body>
 </html>
