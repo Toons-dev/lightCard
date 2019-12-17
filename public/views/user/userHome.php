30decb99486863e11c068e5af9accd9dea72d93b
Toons-dev <contact.debuire@gmail>
2019-12-17 12:08:11 +0100

    affichage des fiches selon utilisateurs

--- public/views/user/userHome.php
+++ public/views/user/userHome.php
@@ -5,22 +5,22 @@ ob_start(); ?>
         <h1>Bonjour <?= $_SESSION['usr_connexion']['usr_firstname'] ?>,</h1>
     </div>
     
+    <?php if(isset($fiches)): ?>
     <h2>Vous pouvez gérer vos propres fiches</h2>
-    <div class="fiche row">
+    <div class="row">
         <?php foreach($fiches as $fiche): ?>
-            <div class="col-12 col-md-6 col-lg-4 fiche">
-                <img src="<?= $fiche['f_media'] ?>" alt="" style="width: 100%;">
+            <div class="col-12 col-md-6 col-lg-2 fiche">
                     <div class="text">
-                        <h2><?= $fiche['f_titre'] ?></h2>
+                        <h6><?= resume($fiche['f_titre'], 30) ?></h6>
                         <p><?= $fiche['f_categorie'] ?></p>
-                        <p><?= resume($fiche['f_texte']) ?></p>
                         <p><?= $fiche['f_date'] ?></p>
-                        <p><?= $fiche['f_link'] ?></p>
-                        <a class="lien" href="<?= url('fiche/'.$fiche['f_id']) ?>">Découvrir</a>
+                        <a class="lien" href="<?= url('fiche/'.$fiche['f_id']) ?>">Aperçu</a>
+                        <a class="lien" href="<?= url('fiche/delete'.$fiche['f_id']) ?>">Supprimer</a>
                     </div>
             </div>
         <?php endforeach; ?>
     </div>
+    <?php endif; ?>
 
     <h2>Créer une nouvelle fiche</h2>
     <div class="fiche">
